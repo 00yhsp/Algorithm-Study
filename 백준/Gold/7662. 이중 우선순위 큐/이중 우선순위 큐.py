bf6e88df7e33bbf74bc74dd735f39fa8328f84d8
Hyeonsoo Park <55729213+00yhsp@users.noreply.github.com>
2024-04-23 11:55:22 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 7488 ms, Memory: 291212 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.py
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.py
@@ -7,27 +7,27 @@ for _ in range(t):
     min_heap = []
     max_heap = []
     k = int(input())
-    deleted = [1] * k
+    deleted = [False] * k
     for i in range(k):
         c, n = input().split()
         n = int(n)
-        
+
         if c == "I":
             heapq.heappush(min_heap, (n, i))
             heapq.heappush(max_heap, (-n, i))
         else:
             if n == -1:
                 if min_heap:
-                    deleted[heapq.heappop(min_heap)[1]] = 0
+                    deleted[heapq.heappop(min_heap)[1]] = True
             elif n == 1:
                 if max_heap:
-                    deleted[heapq.heappop(max_heap)[1]] = 0
-        
-        while min_heap and deleted[min_heap[0][1]] == 0:
+                    deleted[heapq.heappop(max_heap)[1]] = True
+
+        while min_heap and deleted[min_heap[0][1]]:
             heapq.heappop(min_heap)
-        while max_heap and deleted[max_heap[0][1]] == 0:
+        while max_heap and deleted[max_heap[0][1]]:
             heapq.heappop(max_heap)
-        
+
 
     if len(min_heap) == 0:
         print("EMPTY")
