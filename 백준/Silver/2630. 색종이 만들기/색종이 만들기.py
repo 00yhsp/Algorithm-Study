6825c6d4797bd0ee6d4b922e1de97518a92e5db8
Hyeonsoo Park <55729213+00yhsp@users.noreply.github.com>
2024-07-08 19:55:58 +0900

    [Silver II] Title: 색종이 만들기, Time: 124 ms, Memory: 111784 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -8,22 +8,22 @@ n = int(input())
 board = [list(map(int, input().split())) for _ in range(n)]
 
 
-def check(rs, cs, re, ce):
+def check(rs, cs, size):
     global blue, white
     color = board[rs][cs]
-    for i in range(rs, re):
-        for j in range(cs, ce):
+    for i in range(rs, rs + size):
+        for j in range(cs, cs + size):
             if board[i][j] != color:
                 return False
     if color == 1:
         blue += 1
     else:
-        white += 1 
+        white += 1
     return True
 
 
 def recursion(rs, cs, size):
-    if check(rs, cs, rs + size, cs + size):
+    if check(rs, cs, size):
         return
     half = size // 2
     recursion(rs, cs, half)
