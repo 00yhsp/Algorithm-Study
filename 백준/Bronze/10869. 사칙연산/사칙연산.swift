d13742dfa8d26d2d76a9d0d9dd7584a04b48f9f5
Hyeonsoo Park <55729213+00yhsp@users.noreply.github.com>
2024-12-29 17:23:00 +0900

    [Bronze V] Title: 사칙연산, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.swift
@@ -0,0 +1,7 @@
+let arr = readLine()!.split(separator: " ").map { Int($0)! }
+
+print(arr[0] + arr[1])
+print(arr[0] - arr[1])
+print(arr[0] * arr[1])
+print(arr[0] / arr[1])
+print(arr[0] % arr[1])
