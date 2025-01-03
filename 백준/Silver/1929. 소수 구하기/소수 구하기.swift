9cb8a669b580a56437bb8610e4ce0c0fc734e24d
Hyeonsoo Park <55729213+00yhsp@users.noreply.github.com>
2025-01-03 16:33:18 +0900

    [Silver III] Title: 소수 구하기, Time: 68 ms, Memory: 80472 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.swift
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.swift
@@ -1,13 +1,20 @@
+import Foundation
+
 let input = readLine()!.split(separator: " ").map { Int($0)! }
 let m = input[0], n = input[1]
+
+if n < 2 { exit(0) }
 
 var prime = [Bool](repeating: true, count: n + 1)
 prime[1] = false
 
-for i in 2...n {
-    for j in i...n {
-        if i * j > n { break }
-        prime[i * j] = false
+let limit = max(Int(sqrt(Double(n))), 2)
+
+for i in 2...limit {
+    if prime[i] {
+        for j in stride(from: i * i, through: n, by: i) {
+            prime[j] = false
+        }
     }
 }
 
