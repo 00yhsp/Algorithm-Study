95928d3e35b347f092535d9e7bf4baf54185a755
Hyeonsoo Park <55729213+00yhsp@users.noreply.github.com>
2025-01-15 17:15:11 +0900

    [Gold IV] Title: DSLR, Time: 5944 ms, Memory: 69652 KB -BaekjoonHub

---
+++ 백준/Gold/9019. DSLR/DSLR.swift
@@ -0,0 +1,67 @@
+let t = Int(readLine()!)!
+
+let operations = [
+    (d, "D", 1),
+    (s, "S", 2),
+    (l, "L", 3),
+    (r, "R", 4)
+]
+
+var visited = Array(repeating: false, count: 10000)
+
+for _ in 0..<t {
+    visited = Array(repeating: false, count: 10000)
+    solution()
+}
+
+func solution() {
+    let ab = readLine()!.split(separator: " ").map { Int($0)! }
+    let a = ab[0], b = ab[1]
+    print(bfs(a, b))
+}
+
+func bfs(_ start: Int, _ end: Int) -> String {
+    var queue = Queue<Int>()
+    queue.enqueue((start, 0))
+    visited[start] = true
+
+    while !queue.isEmpty {
+        let (node, path) = queue.dequeue()!
+        if node == end { return itoa(path) }
+        for (op, _, idx) in operations {
+            let next = op(node)
+            if !visited[next] {
+                visited[next] = true
+                queue.enqueue((next, path * 10 + idx))
+            }
+        }
+    }
+
+    return ""
+}
+
+func itoa(_ n: Int) -> String {
+    var str = ""
+    for i in String(n) {
+        str += operations[Int(String(i))! - 1].1
+    }
+    return str
+}
+
+func d(_ n: Int) -> Int { (2 * n) % 10000 }
+func s(_ n: Int) -> Int { return n == 0 ? 9999 : n - 1 }
+func l(_ n: Int) -> Int { return (n % 1000) * 10 + n / 1000 }
+func r(_ n: Int) -> Int { return (n % 10) * 1000 + n / 10 }
+
+struct Queue<T> {
+    typealias Element = (value: T, path: Int)
+    private var queue = [Element]()
+    private var front = 0
+    var isEmpty: Bool { front >= queue.count }
+    mutating func enqueue(_ element: Element) { queue.append(element) }
+    mutating func dequeue() -> Element? {
+        let value = queue[front]
+        front += 1
+        return value
+    }
+}
