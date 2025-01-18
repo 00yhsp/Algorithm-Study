25573c38cfb784bc4f661e8e64cb4013ea9cb2d1
Hyeonsoo Park <55729213+00yhsp@users.noreply.github.com>
2025-01-18 20:59:58 +0900

    [Gold V] Title: 최소비용 구하기, Time: 152 ms, Memory: 77000 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.swift
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.swift
@@ -17,32 +17,100 @@ print(dijkstra(start, end))
 
 func dijkstra(_ start: Int, _ end: Int) -> Int {
     var distance = Array(repeating: INF, count: n + 1)
-    var visited = Array(repeating: false, count: n + 1)
+    var heap = Heap<(weight: Int, node: Int)>(priority: { $0.weight < $1.weight })
+
     distance[start] = 0
+    heap.push((weight: 0, node: start))
 
-    for i in 1...n { graph[i][i] = 0 }
-    for _ in 1...n {
-        var minDistance = INF
-        var currentNode = -1
+    while !heap.isEmpty {
+        let (currentWeight, currentNode) = heap.pop()!
 
-        for i in 1...n {
-            if !visited[i] && distance[i] < minDistance {
-                minDistance = distance[i]
-                currentNode = i
-            }
-        }
-
-        if currentNode == -1 { break }
-
-        visited[currentNode] = true
+        if currentWeight > distance[currentNode] { continue }
 
         for nextNode in 1...n {
-            if visited[nextNode] { continue }
-            let newDistance = distance[currentNode] + graph[currentNode][nextNode]
-            if newDistance < distance[nextNode] { distance[nextNode] = newDistance }
+            let newDistance = currentWeight + graph[currentNode][nextNode]
+            if newDistance < distance[nextNode] {
+                distance[nextNode] = newDistance
+                heap.push((weight: newDistance, node: nextNode))
+            }
         }
-
     }
 
     return distance[end]
+}
+
+struct Heap<T> {
+    private var elements: [T]
+    private let priority: (T, T) -> Bool
+
+    var isEmpty: Bool { elements.isEmpty }
+
+    var count: Int { elements.count }
+
+    var peek: T? { elements.first }
+
+    init(elements: [T] = [], priority: @escaping (T, T) -> Bool) {
+        self.elements = elements
+        self.priority = priority
+
+        if !elements.isEmpty {
+            for i in stride(from: elements.count / 2 - 1, through: 0, by: -1) {
+                siftDown(i)
+            }
+        }
+    }
+
+    mutating func push(_ element: T) {
+        elements.append(element)
+        siftUp(elements.count - 1)
+    }
+
+    @discardableResult
+    mutating func pop() -> T? {
+        guard !elements.isEmpty else { return nil }
+
+        elements.swapAt(0, elements.count - 1)
+        let popped = elements.removeLast()
+
+        if !elements.isEmpty { siftDown(0) }
+        return popped
+    }
+
+    private mutating func siftUp(_ index: Int) {
+        var childIndex = index
+        let child = elements[childIndex]
+
+        while childIndex > 0 {
+            let parentIndex = (childIndex - 1) / 2
+            let parent = elements[parentIndex]
+
+            if priority(parent, child) { break }
+
+            elements[childIndex] = parent
+            childIndex = parentIndex
+        }
+        elements[childIndex] = child
+    }
+
+    private mutating func siftDown(_ index: Int) {
+        var parentIndex = index
+        let count = elements.count
+
+        while true {
+            let leftIndex = parentIndex * 2 + 1
+            let rightIndex = parentIndex * 2 + 2
+            var candidateIndex = parentIndex
+
+            if leftIndex < count && priority(elements[leftIndex], elements[candidateIndex]) {
+                candidateIndex = leftIndex
+            }
+            if rightIndex < count && priority(elements[rightIndex], elements[candidateIndex]) {
+                candidateIndex = rightIndex
+            }
+            if candidateIndex == parentIndex { break }
+
+            elements.swapAt(parentIndex, candidateIndex)
+            parentIndex = candidateIndex
+        }
+    }
 }
