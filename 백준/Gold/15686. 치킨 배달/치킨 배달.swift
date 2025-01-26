ad57ca5775d562f9344b34c011ccad71e4e7c0d3
Hyeonsoo Park <55729213+00yhsp@users.noreply.github.com>
2025-01-27 01:43:23 +0900

    [Gold V] Title: 치킨 배달, Time: 16 ms, Memory: 69108 KB -BaekjoonHub

---
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.swift
@@ -0,0 +1,48 @@
+let nm = readLine()!.split(separator: " ").map { Int($0)! }
+let n = nm[0], m = nm[1]
+
+var arr = [[Int]]()
+var houses = [(Int, Int)]()
+var chickens = [(Int, Int)]()
+
+for i in 0..<n {
+    let row = readLine()!.split(separator: " ").map { Int($0)! }
+    arr.append(row)
+    for j in 0..<n {
+        if row[j] == 1 { houses.append((i, j)) }
+        else if row[j] == 2 { chickens.append((i, j)) }
+    }
+}
+
+var minDistance = Int.max
+var selected = Array(repeating: false, count: chickens.count)
+
+backtrack(0, 0)
+print(minDistance)
+
+func backtrack(_ start: Int, _ count: Int) {
+    if count == m {
+        var totalDistance = 0
+
+        for house in houses {
+            var houseMinDistance = Int.max
+            for i in 0..<chickens.count {
+                if selected[i] {
+                    houseMinDistance = min(houseMinDistance, abs(house.0 - chickens[i].0) + abs(house.1 - chickens[i].1))
+                }
+            }
+            totalDistance += houseMinDistance
+        }
+
+        minDistance = min(minDistance, totalDistance)
+        return
+    }
+
+    for i in start..<chickens.count {
+        if !selected[i] {
+            selected[i] = true
+            backtrack(i + 1, count + 1)
+            selected[i] = false
+        }
+    }
+}
