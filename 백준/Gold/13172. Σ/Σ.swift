bf5b476eec3847a6f96e10a7591c8e4c64df77f6
Hyeonsoo Park <55729213+00yhsp@users.noreply.github.com>
2025-02-11 21:45:17 +0900

    [Gold IV] Title: Σ, Time: 28 ms, Memory: 69104 KB -BaekjoonHub

---
+++ 백준/Gold/13172. Σ/Σ.swift
@@ -0,0 +1,37 @@
+let mod = 1_000_000_007
+let t = Int(readLine()!)!
+var result = 0
+
+for _ in 0..<t {
+    let input = readLine()!.split(separator: " ").map { Int($0)! }
+    var (n, s) = (input[0], input[1])
+
+    let g = gcd(n, s)
+    n /= g
+    s /= g
+
+    let invN = modExp(n, mod - 2)
+
+    result = (result + (s * invN) % mod) % mod
+}
+
+print(result)
+
+func modExp(_ base: Int, _ exp: Int) -> Int {
+    if exp == 0 { return 1 }
+    if exp == 1 { return base % mod }
+
+    let half = modExp(base, exp / 2)
+    let halfSquared = (half * half) % mod
+
+    if exp % 2 == 0 {
+        return halfSquared
+    } else {
+        return (halfSquared * base) % mod
+    }
+}
+
+func gcd(_ a: Int, _ b: Int) -> Int {
+    if b == 0 { return a }
+    return gcd(b, a % b)
+}
