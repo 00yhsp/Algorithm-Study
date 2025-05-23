26f34d7d3ec8f72e749369eb08d0206c1919acc9
Hyeonsoo Park <55729213+00yhsp@users.noreply.github.com>
2025-05-24 01:58:17 +0900

    [level 1] Title: K번째수, Time: 0.18 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.swift
+++ 프로그래머스/1/42748. K번째수/K번째수.swift
@@ -2,11 +2,9 @@ import Foundation
 
 func solution(_ array:[Int], _ commands:[[Int]]) -> [Int] {
     var result = [Int]()
-    
     for com in commands {
-        let partial = array[com[0] - 1...com[1] - 1].sorted()
-        result.append(partial[com[2] - 1])    
+        var subarr = array[com[0] - 1..<com[1]].sorted()
+        result.append(subarr[com[2] - 1])
     }
-    
     return result
 }
