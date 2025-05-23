474a137a435dec1a81123b125961e9c6cf6472d6
Hyeonsoo Park <55729213+00yhsp@users.noreply.github.com>
2025-05-24 00:36:04 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 3.28 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.swift
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.swift
@@ -2,73 +2,79 @@ import Foundation
 
 func solution(_ jobs:[[Int]]) -> Int {
     let count = jobs.count
-    var jobs = (0..<jobs.count)
-                .map { (jobs[$0][0], jobs[$0][1], $0) }
-                .sorted { $0.0 > $1.0 }
-    var time = 0
-    var totalTurnAroundTime = 0
-    var heap = Heap { 
-        if $0.time == $1.time {
-            if $0.requestTime == $1.requestTime {
-                return $0.no < $1.no
+    var returnTimes = [Int]()
+    var currentTime = 0
+    var heap = Heap((0..<count).map { (jobs[$0][0], jobs[$0][1], $0) } ) {
+        if $0.1 == $1.1 { 
+            if $0.0 == $1.0 { return $0.2 < $1.2 }
+            else { return $0.0 < $1.0 }
+        }
+        else { return $0.1 < $1.1 }
+    }
+    while !heap.isEmpty {
+        var temp = [(Int, Int, Int)]()
+        var flag = false
+        
+        while !heap.isEmpty {
+            let popped = heap.pop()!
+            if popped.0 <= currentTime {
+                currentTime += popped.1
+                returnTimes.append(currentTime - popped.0)
+                flag = true
+                break
             } else {
-                return $0.requestTime < $1.requestTime
+                temp.append(popped)
             }
-        } else {
-            return $0.time < $1.time
         }
+        if !flag, let min = temp.min(by: { $0.0 < $1.0 }) { 
+            currentTime = min.0 + min.1
+            returnTimes.append(min.1)
+            if let firstIdx = temp.firstIndex { elem in
+                elem.0 == min.0 && elem.1 == min.1 && elem.2 == min.2 
+            } {
+              temp.remove(at: firstIdx)    
+            }
+        }
+        for t in temp { heap.push(t) }
     }
     
-    while !jobs.isEmpty || !heap.isEmpty {
-        while let last = jobs.last, last.0 <= time {
-            heap.push(jobs.removeLast())
-        }
-        if let popped = heap.pop() {
-            time += popped.time
-            totalTurnAroundTime += time - popped.requestTime
-        } else if let last = jobs.last {
-            time = last.0
-        }
-    }
-    
-    return totalTurnAroundTime / count
+    return returnTimes.reduce(0, +) / returnTimes.count
 }
 
 struct Heap {
-    var elements: [(requestTime: Int, time: Int, no: Int)]
-    var priority: ((requestTime: Int, time: Int, no: Int), (requestTime: Int, time: Int, no: Int)) -> Bool
+    var elements: [(Int, Int, Int)]
+    let priority: ((Int, Int, Int), (Int, Int, Int)) -> Bool
     
+    var count: Int { elements.count }
     var isEmpty: Bool { elements.isEmpty }
-    var peek: (requestTime: Int, time: Int, no: Int)? { elements.first }
     
-    init(elements: [(requestTime: Int, time: Int, no: Int)] = [], priority: @escaping ((requestTime: Int, time: Int, no: Int), (requestTime: Int, time: Int, no: Int)) -> Bool) {
+    init(_ elements: [(Int, Int, Int)], priority: @escaping ((Int, Int, Int), (Int, Int, Int)) -> Bool) {
         self.elements = elements
         self.priority = priority
-        
-        if !isEmpty { heapify() }
+        heapify()
     }
     
     mutating func heapify() {
-        for i in stride(from: elements.count / 2 - 1, through: 0, by: -1) {
-            siftDown(i)
-        }
+        guard !isEmpty else { return }
+        for i in stride(from: count / 2 - 1, through: 0, by: -1) { siftDown(i) }
     }
     
-    mutating func push(_ element: (requestTime: Int, time: Int, no: Int)) {
+    mutating func push(_ element: (Int, Int, Int)) {
         elements.append(element)
-        siftUp(elements.count - 1)
+        siftUp(count - 1)
     }
     
-    mutating func pop() -> (requestTime: Int, time: Int, no: Int)? {
+    @discardableResult
+    mutating func pop() -> (Int, Int, Int)? {
         guard !isEmpty else { return nil }
-        elements.swapAt(0, elements.count - 1)
+        elements.swapAt(0, count - 1)
         let popped = elements.removeLast()
         if !isEmpty { siftDown(0) }
         return popped
     }
     
-    mutating func siftUp(_ index: Int) {
-        var childIndex = index
+    mutating func siftUp(_ idx: Int) {
+        var childIndex = idx
         let child = elements[childIndex]
         
         while childIndex > 0 {
@@ -83,28 +89,23 @@ struct Heap {
         elements[childIndex] = child
     }
     
-    mutating func siftDown(_ index: Int) {
-        var parentIndex = index
-        let count = elements.count
-        let parent = elements[parentIndex]
-
+    mutating func siftDown(_ idx: Int) {
+        var parentIndex = idx
+        
         while true {
             let leftIndex = parentIndex * 2 + 1
             let rightIndex = parentIndex * 2 + 2
             var candidateIndex = parentIndex
-
+            
             if leftIndex < count, priority(elements[leftIndex], elements[candidateIndex]) {
                 candidateIndex = leftIndex
             }
             if rightIndex < count, priority(elements[rightIndex], elements[candidateIndex]) {
                 candidateIndex = rightIndex
             }
-            if parentIndex == candidateIndex { break }
-
-            elements[parentIndex] = elements[candidateIndex]
+            if candidateIndex == parentIndex { break }
+            elements.swapAt(candidateIndex, parentIndex)
             parentIndex = candidateIndex
         }
-
-        elements[parentIndex] = parent
     }
 }
