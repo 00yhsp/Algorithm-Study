ebe151c4c58e70a13a5784598dc4d691c47a94ba
Hyeonsoo Park <55729213+00yhsp@users.noreply.github.com>
2025-05-24 16:10:27 +0900

    [Silver IV] Title: 수 찾기, Time: 144 ms, Memory: 80096 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.swift
+++ 백준/Silver/1920. 수 찾기/수 찾기.swift
@@ -1,9 +1,22 @@
 let n = Int(readLine()!)!
-let set = Set(readLine()!.split(separator: " ").map { Int($0)! })
+let arr = readLine()!.split(separator: " ").compactMap { Int($0) }.sorted()
+let _ = readLine()
+let targets = readLine()!.split(separator: " ").compactMap { Int($0) }
 
-let m = Int(readLine()!)
-let arr = readLine()!.split(separator: " ").map { Int($0)! }
+var output = [String]()
+targets.forEach { output.append(binarySearch($0)) }
+print(output.joined(), terminator: "")
 
-for elem in arr {
-    set.contains(elem) ? print("1") : print("0")
+func binarySearch(_ x: Int) -> String {
+    var start = 0
+    var end = n - 1
+
+    while start <= end {
+        let mid = (start + end) / 2
+        if arr[mid] < x { start = mid + 1 }
+        else { end = mid - 1 }
+        if arr[mid] == x { return "1\n" }
+    }
+    
+    return "0\n"
 }
