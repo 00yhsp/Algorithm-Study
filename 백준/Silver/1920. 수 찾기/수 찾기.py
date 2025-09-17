f3e2daf6904bdecef1f688cc3e377e603a1e18e2
Hyeonsoo Park <55729213+00yhsp@users.noreply.github.com>
2025-09-18 01:56:03 +0900

    [Silver IV] Title: 수 찾기, Time: 180 ms, Memory: 145308 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,28 +1,6 @@
-import sys
-
-input = sys.stdin.readline
-
-n = int(input())
-
-a = sorted(list(map(int, input().split())))
-
-m = int(input())
-
-m_arr = list(map(int, input().split()))
-
-
-def binary_search(s, e, v):
-    m = (s + e) // 2
-    if v == a[m]:
-        return 1
-    if s >= e:
-        return 0
-    if v < a[m]:
-        return binary_search(s, m - 1, v)
-    if v > a[m]:
-        return binary_search(m + 1, e, v)
-
-
-
-for elem in m_arr:
-    print(binary_search(0, n - 1, elem))
+input()
+s = set(map(int, input().split()))
+input()
+arr = list(map(int, input().split()))
+for elem in arr:
+    print(1 if elem in s else 0)
