bbb7f6244ac82792063511ec2ab67480827394e9
Hyeonsoo Park <55729213+00yhsp@users.noreply.github.com>
2025-09-18 01:32:46 +0900

    [Bronze I] Title: 팰린드롬수, Time: 4 ms, Memory: 69096 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.swift
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.swift
@@ -1,16 +1,13 @@
 while true {
-    let str = Array(readLine()!)
-    if str == ["0"] { break }
+    let arr = Array(readLine()!)
+    if arr == ["0"] { break }
+    let count = arr.count
     var flag = true
-    for i in 0..<str.count / 2 {
-        if str[i] != str[str.count - i - 1] {
+    for i in (0..<count / 2) {
+        if arr[i] != arr[count - i - 1] { 
             flag = false
             break
         }
     }
-    if flag {
-        print("yes")
-    } else {
-        print("no")
-    }
+    print(flag ? "yes" : "no")
 }
