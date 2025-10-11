e8e64c63035d341496dc64912176975e8dbd09a4
Hyeonsoo Park <55729213+00yhsp@users.noreply.github.com>
2025-10-11 10:27:07 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.swift
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.swift
@@ -1,16 +1,14 @@
-var dp = [Int](repeating: 0, count: 11)
-
+var dp = [Int](repeating: 0, count: 12)
 dp[1] = 1
 dp[2] = 2
 dp[3] = 4
 
-for i in 4...10 {
-    dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1]
+for i in 4...11 {
+    dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
 }
 
-let t = Int(readLine()!)!
-
-for _ in 0..<t {
+let T = Int(readLine()!)!
+for _ in 0..<T {
     let n = Int(readLine()!)!
     print(dp[n])
 }
