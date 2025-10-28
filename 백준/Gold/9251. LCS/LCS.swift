8c2a0fed3f3561264acab2d5613b5e25900bb9a9
Hyeonsoo Park <55729213+00yhsp@users.noreply.github.com>
2025-10-28 16:13:02 +0900

    [Gold V] Title: LCS, Time: 28 ms, Memory: 69096 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.swift
+++ 백준/Gold/9251. LCS/LCS.swift
@@ -1,20 +1,18 @@
-let s1 = Array(readLine()!)
-let s2 = Array(readLine()!)
+let str1 = Array(readLine()!)
+let str2 = Array(readLine()!)
 
-let n = s1.count
-let m = s2.count
+let len1 = str1.count
+let len2 = str2.count
 
-var previous = Array(repeating: 0, count: m + 1)
-var current = Array(repeating: 0, count: m + 1)
-for i in 1...n {
-    for j in 1...m {
-        if s1[i - 1] == s2[j - 1] {
-            current[j] = previous[j - 1] + 1
-        } else {
-            current[j] = max(previous[j], current[j - 1])
-        }
+var previous = [Int](repeating: 0, count: len2 + 1)
+var current = [Int](repeating: 0, count: len2 + 1)
+
+for i in 1...len1 {
+    for j in 1...len2 {
+        if str1[i - 1] == str2[j - 1] { current[j] = previous[j - 1] + 1 }
+        else { current[j] = max(previous[j], current[j - 1]) }
     }
     previous = current
 }
 
-print(current[m])
+print(current[len2])
