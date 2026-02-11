587099dd721bdc211b37f13213ef25517bc4bda0
Hyeonsoo Park <55729213+00yhsp@users.noreply.github.com>
2026-02-11 19:18:02 +0900

    [Bronze III] Title: 직각삼각형, Time: 8 ms, Memory: 69108 KB -BaekjoonHub

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.swift
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.swift
@@ -1,13 +1,8 @@
-import Foundation
-
 while true {
-    let arr = readLine()!.split(separator: " ").map { Double($0)! }.sorted()
-    let (a, b, c) = (arr[0], arr[1], arr[2])
-    if a == 0 && b == 0 && c == 0 { break }
+    var arr = readLine()!.split(separator: " ").compactMap { Int($0) }
+    if arr[0] == 0 && arr[1] == 0 && arr[2] == 0 { break }
 
-    if (pow(a + b, 2) - (2 * a * b) == pow(c, 2)) && !(a == 1 && b == 1 && c == 1) {
-        print("right")
-    } else {
-        print("wrong")
-    }
+    arr.sort(by: <)
+    if arr[0] * arr[0] + arr[1] * arr[1] == arr[2] * arr[2] { print("right") }
+    else { print("wrong") }
 }
