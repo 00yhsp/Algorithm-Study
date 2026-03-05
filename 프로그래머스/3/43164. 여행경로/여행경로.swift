bd413398d1f467767220ae253479201af7d6cab5
Hyeonsoo Park <55729213+00yhsp@users.noreply.github.com>
2026-03-05 10:50:51 +0900

    [level 3] Title: 여행경로, Time: 0.20 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.swift
+++ 프로그래머스/3/43164. 여행경로/여행경로.swift
@@ -1,28 +1,28 @@
-func solution(_ tickets: [[String]]) -> [String] {
-    let ticketCount = tickets.count
-    var tickets = tickets.sorted {
-        if $0[0] == $1[0] { return $0[1] < $1[1] }
-        return $0[0] < $1[0]
-    }
+import Foundation
+
+func solution(_ tickets:[[String]]) -> [String] {
+    let sortedTickets = tickets.sorted { $0[1] < $1[1] }
     
-    var visited = [Bool](repeating: false, count: ticketCount)
-    var answer = [String]()
-    
-    func dfs(_ airport: String, _ route: [String]) {
-        if !answer.isEmpty { return }
-        if route.count == ticketCount + 1 {
-            answer = route
-            return
+    var visited = Array(repeating: false, count: sortedTickets.count)
+    var result = [String]()
+
+    func dfs(_ now: String, _ path: [String]) -> Bool {
+        if path.count == sortedTickets.count + 1 {
+            result = path
+            return true
         }
-        for i in 0..<ticketCount {
-            if !visited[i] && tickets[i][0] == airport {
-                visited[i] = true
-                dfs(tickets[i][1], route + [tickets[i][1]])
-                visited[i] = false
+
+        for i in 0..<sortedTickets.count where !visited[i] && sortedTickets[i][0] == now {
+            visited[i] = true
+            if dfs(sortedTickets[i][1], path + [sortedTickets[i][1]]) {
+                return true
             }
+            visited[i] = false
         }
+        return false
     }
-    
+
     dfs("ICN", ["ICN"])
-    return answer
+    
+    return result
 }
