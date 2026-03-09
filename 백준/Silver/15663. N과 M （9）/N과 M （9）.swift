eebce861fe6f4e666d2e46384e6cc613bb1f8f98
Hyeonsoo Park <55729213+00yhsp@users.noreply.github.com>
2026-03-09 12:34:42 +0900

    [Silver II] Title: N과 M (9), Time: 36 ms, Memory: 70232 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.swift
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.swift
@@ -1,25 +1,27 @@
-let nm = readLine()!.split(separator: " ").map { Int($0)! }
-let n = nm[0], m = nm[1]
-let arr = readLine()!.split(separator: " ").map { Int($0)! }.sorted()
-var visited = Array(repeating: false, count: n + 1)
-var nodes = [Int]()
+let nm = readLine()!.split(separator: " ").compactMap { Int($0) }
+let (n, m) = (nm[0], nm[1])
+var sorted = readLine()!.split(separator: " ").compactMap { Int($0) }.sorted()
+var visited = [Bool](repeating: false, count: n)
 
-backtrack()
+var arr = [Int](repeating: 0, count: m)
+var output = [String]()
 
-func backtrack() {
-    if nodes.count == m {
-        print(nodes.map { String($0) }.joined(separator: " "))
+func dfs(_ depth: Int) {
+    if depth == m {
+        output.append(arr.map(String.init).joined(separator: " "))
         return
     }
-    var last = 0
-    for i in 0..<n {
-        if !visited[i] && last != arr[i]  {
-            visited[i] = true
-            last = arr[i]
-            nodes.append(arr[i])
-            backtrack()
-            nodes.removeLast()
-            visited[i] = false
-        }
+
+    var prev = -1
+
+    for i in 0..<n where !visited[i] && prev != sorted[i] {
+        visited[i] = true
+        prev = sorted[i]
+        arr[depth] = sorted[i]
+        dfs(depth + 1)
+        visited[i] = false
     }
 }
+
+dfs(0)
+print(output.joined(separator: "\n"))
