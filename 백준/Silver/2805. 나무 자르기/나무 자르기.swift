9748cc4df8b5395656782ec7e7cbe381cbb26df6
Hyeonsoo Park <55729213+00yhsp@users.noreply.github.com>
2026-03-17 10:39:38 +0900

    [Silver II] Title: 나무 자르기, Time: 544 ms, Memory: 145404 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.swift
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.swift
@@ -1,21 +1,18 @@
-let nm = readLine()!.split(separator: " ").map { Int($0)! }
-let n = nm[0], m = nm[1]
+let nm = readLine()!.split(separator: " ").compactMap { Int($0) }
+let (n, m) = (nm[0], nm[1])
+let arr = readLine()!.split(separator: " ").compactMap { Int($0) }
 
-let arr = readLine()!.split(separator: " ").map { Int($0)! }
+var start = 0; var end = arr.max()!
 
-var start = 0, end = arr.max()!
+while start <= end {
+    let mid = (start + end) / 2
+    let remainder = arr.reduce(0) { $0 + max($1 - mid, 0) } 
 
-binarySearch()
-
-func binarySearch() {
-    while start <= end {
-        let mid = (start + end) / 2
-        let sum = arr.reduce(0) { $0 + ($1 > mid ? $1 - mid : 0) }
-        if sum >= m {
-            start = mid + 1
-        } else {
-            end = mid - 1
-        }
+    if remainder >= m {
+        start = mid + 1
+    } else {
+        end = mid - 1
     }
-    print(end)
 }
+
+print(end)
