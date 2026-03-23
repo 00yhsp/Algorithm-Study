30e6d714d7e179c4fe4f8788ec0a044686585c19
Hyeonsoo Park <55729213+00yhsp@users.noreply.github.com>
2026-03-23 20:40:07 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+print = sys.stdout.write
+
+dp = [0 for i in range(1001)]
+dp[1] = 1
+dp[2] = 3
+
+n = int(input())
+
+if n >= 3:
+    for i in range(3, n + 1):
+        dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % 10007
+
+print(f"{dp[n]}\n")
