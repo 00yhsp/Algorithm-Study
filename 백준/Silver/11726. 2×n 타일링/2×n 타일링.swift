733e2e45a0c6fcf17f7bf39f4b04499abb8cdc04
Hyeonsoo Park <55729213+00yhsp@users.noreply.github.com>
2026-03-23 19:36:10 +0900

    [Silver III] Title: 2×n 타일링, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.swift
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.swift
@@ -1,11 +1,12 @@
-import Foundation
-
 let n = Int(readLine()!)!
-var dp = Array(repeating: 0, count: 1001)
-dp[1] = 1; dp[2] = 2; dp[3] = 3;
-if n < 4 { print(dp[n]); exit(0) }
-for i in 4...n {
-    dp[i] = (dp[i - 1] + dp[i - 2]) % 10007
+var dp = [Int](repeating: 0, count: 1001)
+dp[1] = 1
+dp[2] = 2
+
+if n >= 3 {
+    for i in 3...n {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 10007
+    }
 }
 
 print(dp[n])
