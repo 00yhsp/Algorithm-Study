abe15ea128d2dbcc710031aba0d1c5c74faae345
Hyeonsoo Park <55729213+00yhsp@users.noreply.github.com>
2026-03-23 10:45:53 +0900

    [Silver III] Title: 1로 만들기, Time: 48 ms, Memory: 76916 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.swift
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.swift
@@ -1,21 +1,12 @@
-import Foundation
-
 let n = Int(readLine()!)!
-if n == 1 { print(0); exit(0) }
-if n == 2 { print(1); exit(0) }
-if n == 3 { print(1); exit(0) }
-var dp = Array(repeating: 0, count: n + 1)
-dp[1] = 0; dp[2] = 1; dp[3] = 1
+var dp = [Int](repeating: 0, count: n + 1)
 
-for i in 4...n { getDP(i) }
-print(dp[n])
-
-func getDP(_ n: Int) {
-    dp[n] = dp[n - 1] + 1
-    if n.isMultiple(of: 2) {
-        dp[n] = min(dp[n / 2] + 1, dp[n])
-    }
-    if n.isMultiple(of: 3) {
-        dp[n] = min(dp[n / 3] + 1, dp[n])
+if n >= 2 {
+    for i in 2...n { 
+        dp[i] = dp[i - 1] + 1
+        if i % 2 == 0 { dp[i] = min(dp[i], dp[i / 2] + 1) }
+        if i % 3 == 0 { dp[i] = min(dp[i], dp[i / 3] + 1) }
     }
 }
+
+print(dp[n])
