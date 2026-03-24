25e6415380684d7a468794e3cee6a985f12ee9bd
Hyeonsoo Park <55729213+00yhsp@users.noreply.github.com>
2026-03-24 10:21:50 +0900

    [Silver III] Title: 계단 오르기, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.swift
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.swift
@@ -1,19 +1,16 @@
-import Foundation
-
 let n = Int(readLine()!)!
-
 var arr = [0]
-var dp = Array(repeating: 0, count: n + 1)
-
+var dp = [Int](repeating: 0, count: 10001)
 for _ in 0..<n { arr.append(Int(readLine()!)!) }
 
-if n == 1 { print(arr[1]); exit(0) }
-if n == 2 { print(arr[1] + arr[2]); exit(0) }
-if n == 3 { print(max(arr[1] + arr[3], arr[2] + arr[3])); exit(0) }
-dp[1] = arr[1]; dp[2] = arr[1] + arr[2]; dp[3] = max(arr[1] + arr[3], arr[2] + arr[3])
+dp[1] = arr[1]
 
-for i in 4...n {
-    dp[i] = max(dp[i - 3] + arr[i - 1] + arr[i], dp[i - 2] + arr[i])
+if n >= 2 { dp[2] = arr[1] + arr[2] }
+if n >= 3 { dp[3] = max(arr[1] + arr[3], arr[2] + arr[3]) }
+if n >= 4 {
+    for i in 4...n {
+        dp[i] = max(dp[i - 3] + arr[i - 1] + arr[i], dp[i - 2] + arr[i])
+    }
 }
 
 print(dp[n])
