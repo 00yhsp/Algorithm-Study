350a328e29f69bf6adc0d5d7f8118ca5d61b00c8
Hyeonsoo Park <55729213+00yhsp@users.noreply.github.com>
2026-04-09 10:40:53 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 1151.65 ms, Memory: 192 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.swift
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.swift
@@ -1,30 +1,46 @@
 import Foundation
 
 func solution(_ edges:[[Int]]) -> [Int] {
-    var inputEdges = [Int: Int]()
-    var outputEdges = [Int: Int]()
-    var keys = Set<Int>()
-    var result = [0, 0, 0, 0]
+    var input = [Int: Int]()
+    var output = [Int: Int]()
+    var nodes = Set<Int>()
+    var result = [Int](repeating: 0, count: 4)
+    
     for edge in edges {
-        let output = edge[0], input = edge[1]
-        keys.insert(output); keys.insert(input)
-        outputEdges[output, default: 0] += 1
-        inputEdges[input, default: 0] += 1
+        input[edge[1], default: 0] += 1
+        output[edge[0], default: 0] += 1
+        nodes.insert(edge[0])
+        nodes.insert(edge[1])
     }
     
-    for key in keys {
-        if inputEdges[key, default: 0] == 0 && outputEdges[key, default: 0] >= 2 {
-            result[0] = key
-        }
-        if inputEdges[key, default: 0] <= 2 && outputEdges[key, default: 0] == 0 {
-            result[2] += 1
-        }
-        if inputEdges[key, default: 0] >= 2 && outputEdges[key, default: 0] == 2 {
-            result[3] += 1
+    for node in nodes {
+        // 생성
+        if output[node, default: 0] >= 2 && input[node, default: 0] == 0 {
+            result[0] = node
         }
         
-        result[1] = outputEdges[result[0], default: 0] - (result[2] + result[3])
+        // 도넛
+        // 무조건 input 1 output 1이므로 연결되면 input 2 output 1
+        // 생성 output - (막대 + 8자 개수)
+        
+        // 막대
+        // input 0 output 0 -> input 1 output 0
+        // input 0 output 1 -> input 1 output 1
+        // input 1 output 1 -> input 2 output 1
+        // output 0 노드 1개 존재
+        if output[node, default: 0] == 0 {
+            result[2] += 1
+        }
+        
+        // 8자
+        // input 1 output 1 -> input 2 output 1
+        // input 2 output 2 -> input 3 output 2
+        // input >= 2 output 2 노드 1개 존재
+        if output[node, default: 0] == 2 && input[node, default: 0] >= 2 {
+            result[3] += 1
+        }
     }
-    
+    result[1] = output[result[0], default: 0] - (result[2] + result[3])
+        
     return result
 }
