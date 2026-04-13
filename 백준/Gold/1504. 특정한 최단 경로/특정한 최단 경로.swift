29424a643cc1c74a04767c4b8141acd828b00b38
Hyeonsoo Park <55729213+00yhsp@users.noreply.github.com>
2026-04-13 22:38:47 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 232 ms, Memory: 76792 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.swift
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.swift
@@ -1,90 +1,98 @@
-let ne = readLine()!.split(separator: " ").map { Int($0)! }
-let n = ne[0], e = ne[1]
-let INF = Int.max / 2
+typealias Element = (node: Int, dist: Int)
+let INF = 200_000_001
 
-var graph = Array(repeating: [(Int, Int)](), count: n + 1)
+let ne = readLine()!.split(separator: " ").compactMap { Int($0) }
+let (n, e) = (ne[0], ne[1])
 
+var graph = [Int: [Element]]()
 for _ in 0..<e {
-    let input = readLine()!.split(separator: " ").map { Int($0)! }
-    let (u, v, w) = (input[0], input[1], input[2])
-    graph[u].append((v, w))
-    graph[v].append((u, w))
+    let uvw = readLine()!.split(separator: " ").compactMap { Int($0) }
+    let (u, v, w) = (uvw[0], uvw[1], uvw[2])
+    graph[u, default: []].append((v, w))
+    graph[v, default: []].append((u, w))
 }
+let n1n2 = readLine()!.split(separator: " ").compactMap { Int($0) }
+let (n1, n2) = (n1n2[0], n1n2[1])
 
-let n1n2 = readLine()!.split(separator: " ").map { Int($0)! }
-let n1 = n1n2[0], n2 = n1n2[1]
+let sDists = dijkstra(1)
+let sToN1 = sDists[n1]
+let sToN2 = sDists[n2]
+let n1Dists = dijkstra(n1)
+let n1ToN2 = n1Dists[n2]
+let n1ToN = n1Dists[n]
+let n2Dists = dijkstra(n2)
+let n2ToN1 = n2Dists[n1]
+let n2ToN = n2Dists[n]
 
-let d1 = safeAdd(dijkstra(1, n1), dijkstra(n1, n2), dijkstra(n2, n))
-let d2 = safeAdd(dijkstra(1, n2), dijkstra(n2, n1), dijkstra(n1, n))
+let asc = [sToN1, n1ToN2, n2ToN]
+let desc = [sToN2, n2ToN1, n1ToN]
+let ascSum = asc.reduce(0, +)
+let descSum = desc.reduce(0, +)
 
-if d1 != -1 && d2 != -1 {
-    print(min(d1, d2))
+if ascSum < descSum {
+    if asc.contains(INF) { print(-1) }
+    else { print(ascSum) }
 } else {
-    print(max(d1, d2))
+    if desc.contains(INF) { print(-1) }
+    else { print(descSum) }
 }
 
-func dijkstra(_ start: Int, _ end: Int) -> Int {
-    var distance = Array(repeating: INF, count: n + 1)
-    distance[start] = 0
-    var heap = Heap<(weight: Int, node: Int)>(priority: <)
-    heap.push((0, start))
+func dijkstra(_ start: Int) -> [Int] {
+    var distances = [Int](repeating: INF, count: n + 1)
+    distances[start] = 0
+    var heap = Heap { $0.dist < $1.dist }
+    heap.push((start, 0))
 
     while !heap.isEmpty {
-        let (currentWeight, currentNode) = heap.pop()!
-        if distance[currentNode] < currentWeight { continue }
+        let (currentNode, currentDist) = heap.pop()!
+        if distances[currentNode] < currentDist { continue }
 
-        for (nextNode, nextWeight) in graph[currentNode] {
-            if currentWeight > Int.max - nextWeight { continue }
-            let newDistance = currentWeight + nextWeight
-            if newDistance < distance[nextNode] {
-                distance[nextNode] = newDistance
-                heap.push((newDistance, nextNode))
+        for (nextNode, nextDist) in graph[currentNode, default: []] {
+            let candidateDist = distances[currentNode] + nextDist
+            if distances[nextNode] > candidateDist {
+                distances[nextNode] = candidateDist
+                heap.push((nextNode, candidateDist))
             }
         }
     }
-    return distance[end]
+
+    return distances
 }
 
-func safeAdd(_ n1: Int, _ n2: Int, _ n3: Int) -> Int {
-    if n1 == INF || n2 == INF || n3 == INF { return -1 }
-    else { return n1 + n2 + n3 }
-}
-
-struct Heap<T> {
-    private var elements: [T]
-    private var priority: (T, T) -> Bool
-
+struct Heap {
+    var elements: [Element]
+    var priority: (Element, Element) -> Bool
     var isEmpty: Bool { elements.isEmpty }
     var count: Int { elements.count }
-    var peek: T? { elements.first }
 
-    init(elements: [T] = [], priority: @escaping (T, T) -> Bool) {
+    init(_ elements: [Element] = [], _ priority: @escaping (Element, Element) -> Bool) {
         self.elements = elements
         self.priority = priority
-        if !elements.isEmpty { heapify() }
+        heapify()
     }
 
-    mutating func heapify() {
-        for i in stride(from: elements.count / 2 - 1, through: 0, by: -1) {
-            siftDown(i)
-        }
-    }
-
-    mutating func push(_ element: T) {
+    mutating func push(_ element: Element) {
         elements.append(element)
-        siftUp(elements.count - 1)
+        siftUp(count - 1)
     }
 
-    mutating func pop() -> T? {
+    mutating func pop() -> Element? {
         guard !isEmpty else { return nil }
-        elements.swapAt(0, elements.count - 1)
+        elements.swapAt(0, count - 1)
         let popped = elements.removeLast()
         if !isEmpty { siftDown(0) }
         return popped
     }
 
-    private mutating func siftUp(_ index: Int) {
-        var childIndex = index
+    private mutating func heapify() {
+        guard !isEmpty else { return }
+        for i in stride(from: count / 2 - 1, through: 0, by: -1) {
+            siftDown(i)
+        }
+    }
+
+    private mutating func siftUp(_ x: Int) {
+        var childIndex = x
         let child = elements[childIndex]
 
         while childIndex > 0 {
@@ -92,28 +100,28 @@ struct Heap<T> {
             let parent = elements[parentIndex]
 
             if priority(parent, child) { break }
+
             elements[childIndex] = parent
             childIndex = parentIndex
         }
         elements[childIndex] = child
     }
 
-    private mutating func siftDown(_ index: Int) {
-        var parentIndex = index
-        let count = elements.count
+    private mutating func siftDown(_ x: Int) {
+        var parentIndex = x
 
         while true {
             let leftIndex = parentIndex * 2 + 1
             let rightIndex = parentIndex * 2 + 2
             var candidateIndex = parentIndex
 
-            if leftIndex < count && priority(elements[leftIndex], elements[candidateIndex]) {
+            if leftIndex < count, priority(elements[leftIndex], elements[candidateIndex]) {
                 candidateIndex = leftIndex
             }
-            if rightIndex < count && priority(elements[rightIndex], elements[candidateIndex]) {
+            if rightIndex < count, priority(elements[rightIndex], elements[candidateIndex]) {
                 candidateIndex = rightIndex
             }
-            if parentIndex == candidateIndex { break }
+            if candidateIndex == parentIndex { break }
             elements.swapAt(parentIndex, candidateIndex)
             parentIndex = candidateIndex
         }
